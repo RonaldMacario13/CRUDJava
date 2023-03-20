154d1031c1af7842095709f7a22b877a0e4dfcc2
Rônald Macário <ronaldmacario13@gmail.com>
2023-03-20 02:02:35 -0300

    Adicionando métodos de pesquisar por ID e deletar por ID.

--- src/main/java/com/CRUDJava/CrudJava/controller/ProdutoController.java
+++ src/main/java/com/CRUDJava/CrudJava/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +33,15 @@ public class ProdutoController {
 	
 	//buscarProdutoPorId
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscarProdutoPorId(@PathVariable Long id) {
+		try {
+			return ResponseEntity.ok(servico.buscarProdutoPorId(id));
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
 	@PostMapping
 	public ResponseEntity<?> adicionarProduto(@RequestBody ProdutoDTO produtoDTO) {
 		try {
@@ -57,5 +67,16 @@ public class ProdutoController {
 	}
 	
 	//removerProdutoPorId
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> removerProduto(@PathVariable Long id) {
+		try {
+			servico.removerProduto(id);
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 	
 }
